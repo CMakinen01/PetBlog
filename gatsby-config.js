9521cb40ad8081dc32d18974feaf6787bbaa0e7c
Camden Makinen <camden.makinen@gmail.com>
2024-09-24 21:30:58 -0400

    Next Steps

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,16 @@ module.exports = {
     title: `My Pet Blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "pages",
+        path: "./src/pages/"
+      }
     },
-    __key: "pages"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ]
 };
