650ba48f24b5a0cc6a392e3f47cfbef7f249d6f5
Camden Makinen <camden.makinen@gmail.com>
2024-09-24 23:20:31 -0400

    Added Dynamic Images

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -2,22 +2,34 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.dog_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.dog_image_alt}
+    />
       {children}
     </Layout>
   )
 }
 
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
+  query($id: String!) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        dog_image{
+          childImageSharp {
+            gatsbyImageData
+          }
+        } 
+        dog_image_alt
       }
     }
   }
